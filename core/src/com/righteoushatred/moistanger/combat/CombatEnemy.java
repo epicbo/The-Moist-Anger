35683d31dba3b3fd5e36ad6ccc18ec2209582fd8
DeadLeaves <iron_maiden1111@hotmail.com>
2016-07-20 19:49:06 +0200

    added superclass constructor

--- core/src/com/righteoushatred/moistanger/combat/CombatEnemy.java
+++ core/src/com/righteoushatred/moistanger/combat/CombatEnemy.java
@@ -2,4 +2,10 @@ package com.righteoushatred.moistanger.combat;
 
 public class CombatEnemy extends CombatEntity {
 
+	public CombatEnemy(int basePhysique, int baseNimbleness, int baseJudgement,
+			int baseGlamour, int baseInitiative) {
+		super(basePhysique, baseNimbleness, baseJudgement, baseGlamour, baseInitiative);
+		// TODO Auto-generated constructor stub
+	}
+
 }
