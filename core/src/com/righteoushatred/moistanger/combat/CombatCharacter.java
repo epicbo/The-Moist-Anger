e34764e092ceca78b586a4765dd03897b0ed4e3d
DeadLeaves <iron_maiden1111@hotmail.com>
2016-07-23 17:29:36 +0200

    Moved player character related variables to CombatCharacter

--- core/src/com/righteoushatred/moistanger/combat/CombatCharacter.java
+++ core/src/com/righteoushatred/moistanger/combat/CombatCharacter.java
@@ -1,11 +1,18 @@
 package com.righteoushatred.moistanger.combat;
 
 public class CombatCharacter extends CombatEntity {
-
 	public CombatCharacter(int basePhysique, int baseNimbleness,
-			int baseJudgement, int baseGlamour, int baseInitiative) {
-		super(basePhysique, baseNimbleness, baseJudgement, baseGlamour, baseInitiative);
-		// TODO Auto-generated constructor stub
+			int baseJudgement, int baseGlamour, int baseInitiative,
+			int minSanity, int maxSanity) {
+		super(basePhysique, baseNimbleness, baseJudgement, baseGlamour,
+				baseInitiative);
+		this.minSanity = minSanity;
+		this.maxSanity = maxSanity;
+		sanity = minSanity;
+		endurance = 0;
 	}
-
+	private int minSanity;
+	private int maxSanity;
+	private int sanity;
+	private int endurance;
 }
